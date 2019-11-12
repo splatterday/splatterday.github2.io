ef7f81d632209e03ee75a04ac0fa09b8f2e1fa0c
splatterday <contactjohnlee@gmail.com>
2019-11-12 14:19:29 +0800

    update repo 191112

---
+++ node-demo/app.js
@@ -0,0 +1,7 @@
+let d3 = require('d3')
+let hello = require('./module1.js')
+//console.log(hello)
+
+let fs = require('fs')
+data = fs.readdirSync('/')
+console.log('data:', data)
---
+++ node-demo/module1.js
@@ -0,0 +1,3 @@
+// module.exports allows us to export the thing
+// and import it into another file
+module.exports = "hello world";
